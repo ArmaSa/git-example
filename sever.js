fdeccd8dc00689b88685bcfe5668bf75b6d69aae
AliArena <sialmail67@gmail.com>
2019-05-25 12:07:25 +0430

    const Port is add

--- sever.js
+++ sever.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 app.get('/',(req,res)=>{
     res.send('hello');
 });
 
-app.listen(3000 , ()=>{
-    console.log('server is running in Port 3000 !');
+app.listen(port , ()=>{
+    console.log(`server is running in Port ${port} !`);
 });
